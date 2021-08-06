0eb255758552ceeebc7b562fda9411b513e689b9
Senol Atac <senol.atac.1@gmail.com>
2021-08-06 10:22:20 +0300

    Update RNMoPubRewardedVideo.ts

--- src/RNMoPubRewardedVideo.ts
+++ src/RNMoPubRewardedVideo.ts
@@ -2,7 +2,6 @@ import { NativeEventEmitter, NativeModules, Platform } from "react-native";
 const { RNMoPubRewardedVideo } = NativeModules;
 
 export interface IRNMoPubRewardedVideo {
-    hasRewardedAdForAdUnit: (adUnitId: string) => Promise<boolean>;
     addEventListener: (eventType: string, listener: (args: any) => void) => void;
     initializeRewardedAd: () => void;
     loadRewardedVideoWithUnitID: (adUnitId: string) => void;
@@ -13,16 +12,6 @@ export interface IRNMoPubRewardedVideo {
 const emitter = new NativeEventEmitter(RNMoPubRewardedVideo);
 
 export default {
-    hasRewardedAdForAdUnit: async (adUnitId: string) => {
-        if (Platform.OS === 'ios') {
-            return new Promise((resolve) => {
-                RNMoPubRewardedVideo.hasAdAvailableForAdUnitID(adUnitId, (data: {'Has ad': boolean}) => {
-                    resolve(data['Has ad']);
-                });
-            });
-        }
-        return RNMoPubRewardedVideo.hasRewardedAdForAdUnit(adUnitId);
-    },
     addEventListener: (eventType, listener)  => emitter.addListener(eventType, listener),
     initializeRewardedAd: () => RNMoPubRewardedVideo.initializeRewardedAd(),
     loadRewardedVideoWithUnitID: (adUnitId) => RNMoPubRewardedVideo.loadRewardedVideoWithUnitID(adUnitId),
