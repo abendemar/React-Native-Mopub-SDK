6b0301bfc5f66bdc769ae5212e022bdd466d77c5
Senol Atac <senol.atac.1@gmail.com>
2021-08-12 18:38:15 +0300

    Update RNMoPubInterstitial.m

--- ios/RNMoPubInterstitial.m
+++ ios/RNMoPubInterstitial.m
@@ -5,6 +5,7 @@
 #import <MPGoogleGlobalMediationSettings.h>
 #import <VungleInstanceMediationSettings.h>
 #import <UnityAdsInstanceMediationSettings.h>
+#import <TapjoyGlobalMediationSettings.h>
 
 #import "MoPub.h"
 #import "MPMoPubConfiguration.h"
