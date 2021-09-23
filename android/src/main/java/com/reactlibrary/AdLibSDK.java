ec34355386f7a000de60aa65745b665741fd9c98
Senol Atac <senol.atac.1@gmail.com>
2021-09-23 11:19:07 +0300

    initial

--- android/src/main/java/com/reactlibrary/AdLibSDK.java
+++ android/src/main/java/com/reactlibrary/AdLibSDK.java
@@ -6,6 +6,7 @@ import android.os.Handler;
 import android.util.Log;
 import javax.annotation.Nullable;
 
+import com.applovin.sdk.AppLovinSdk;
 import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
@@ -17,6 +18,10 @@ import com.mopub.common.MoPub;
 import com.mopub.common.SdkConfiguration;
 import com.mopub.common.SdkInitializationListener;
 import com.mopub.common.logging.MoPubLog;
+import com.mopub.mobileads.IronSourceAdapterConfiguration;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class AdLibSDK extends ReactContextBaseJavaModule {
 
@@ -39,7 +44,7 @@ public class AdLibSDK extends ReactContextBaseJavaModule {
     }
 
     @ReactMethod
-    public void initializeSDK(final String unitID, String ironSourceId) {
+    public void initializeSDK(final String unitID, final String ironSourceId) {
         Log.i("Mopub SDK", "Initialization...");
 
         Handler mainHandler = new Handler(getCurrentActivity().getMainLooper());
@@ -48,11 +53,17 @@ public class AdLibSDK extends ReactContextBaseJavaModule {
             @Override
             public void run() {
 
+                // declare your ironSource App Key and Ad Units
+                Map<String, String> ironSourceSettings = new HashMap<>(); ironSourceSettings.put("applicationKey", ironSourceId);
+
                 SdkConfiguration sdkConfiguration = new SdkConfiguration.Builder(unitID)
                         .withLogLevel(MoPubLog.LogLevel.DEBUG)
+                        .withMediatedNetworkConfiguration(IronSourceAdapterConfiguration.class.getName(),
+                                ironSourceSettings)
                         .build();
 
                 MoPub.initializeSdk(getCurrentActivity(), sdkConfiguration, initSdkListener());
+                AppLovinSdk.initializeSdk(getCurrentActivity());
             }
 
             private SdkInitializationListener initSdkListener() {
